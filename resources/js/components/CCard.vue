0ae52a2b27d07107ae8e00cfae1fa2aa995656be
JCPurger <jc.purger@gmail.com>
2019-09-29 01:23:59 -0300

    components indent fixs

--- resources/js/components/CCard.vue
+++ resources/js/components/CCard.vue
@@ -1,102 +1,102 @@
 <template>
 <div>
-    <div class="mt-3">
-        <b-card>
+  <div class="mt-3">
+    <b-card>
+      <b-card-title>Card Title</b-card-title>
+      <b-card-img-lazy src="https://placekitten.com/480/210" alt="Image" height="420"></b-card-img-lazy>
+      <b-card-text>Header and footers variants.</b-card-text>
+      <!-- <b-card-footer>This is a footer</b-card-footer> -->
+    </b-card>
+  </div>
+  <div class="mt-3">
+    <b-card-group deck>
+        <b-card  >
+          <b-card-title>Card Title</b-card-title>
+          <b-card-img-lazy src="https://placekitten.com/480/210" alt="Image" height="200"></b-card-img-lazy>
+          <b-card-text>Header and footers variants.</b-card-text>
+          <!-- <b-card-footer>This is a footer</b-card-footer> -->
+        </b-card>
+
+        <b-card  >
+          <b-card-title>Card Title</b-card-title>
+          <b-card-img-lazy src="https://placekitten.com/480/210" alt="Image" height="200"></b-card-img-lazy>
+          <b-card-text>Header and footers variants.</b-card-text>
+          <!-- <b-card-footer>This is a footer</b-card-footer> -->
+        </b-card>
+    </b-card-group>
+  </div>
+
+  <div class="mt-3">
+    <b-card-group deck>
+        <b-card  >
+          <b-card-title>Card Title</b-card-title>
+          <b-card-img-lazy src="https://placekitten.com/480/210" alt="Image" height="200"></b-card-img-lazy>
+          <b-card-text>Header and footers variants.</b-card-text>
+          <!-- <b-card-footer>This is a footer</b-card-footer> -->
+        </b-card>
+
+        <b-card  >
+          <b-card-title>Card Title</b-card-title>
+          <b-card-img-lazy src="https://placekitten.com/480/210" alt="Image" height="200"></b-card-img-lazy>
+          <b-card-text>Header and footers variants.</b-card-text>
+          <!-- <b-card-footer>This is a footer</b-card-footer> -->
+        </b-card>
+    </b-card-group>
+  </div>
+
+  <div class="mt-3">
+    <b-card-group deck>
+        <b-card  >
+          <b-card-title>Card Title</b-card-title>
+          <b-card-img-lazy src="https://placekitten.com/480/210" alt="Image" height="200"></b-card-img-lazy>
+          <b-card-text>Header and footers variants.</b-card-text>
+          <!-- <b-card-footer>This is a footer</b-card-footer> -->
+        </b-card>
+
+        <b-card  >
+          <b-card-title>Card Title</b-card-title>
+          <b-card-img-lazy src="https://placekitten.com/480/210" alt="Image" height="200"></b-card-img-lazy>
+          <b-card-text>Header and footers variants.</b-card-text>
+          <!-- <b-card-footer>This is a footer</b-card-footer> -->
+        </b-card>
+    </b-card-group>
+  </div>
+
+  <div class="mt-3">
+    <b-card-group deck>
+        <b-card  >
+          <b-card-title>Card Title</b-card-title>
+          <b-card-img-lazy src="https://placekitten.com/480/210" alt="Image" height="200"></b-card-img-lazy>
+          <b-card-text>Header and footers variants.</b-card-text>
+          <!-- <b-card-footer>This is a footer</b-card-footer> -->
+        </b-card>
+
+        <b-card  >
+          <b-card-title>Card Title</b-card-title>
+          <b-card-img-lazy src="https://placekitten.com/480/210" alt="Image" height="200"></b-card-img-lazy>
+          <b-card-text>Header and footers variants.</b-card-text>
+          <!-- <b-card-footer>This is a footer</b-card-footer> -->
+        </b-card>
+    </b-card-group>
+  </div>
+
+  <div class="mt-3">
+    <b-card-group deck>
+        <b-card  >
+          <b-card-title>Card Title</b-card-title>
+          <b-card-img-lazy src="https://placekitten.com/480/210" alt="Image" height="200"></b-card-img-lazy>
+          <b-card-text>Header and footers variants.</b-card-text>
+          <!-- <b-card-footer>This is a footer</b-card-footer> -->
+        </b-card>
+
+        <b-card  >
             <b-card-title>Card Title</b-card-title>
-            <b-card-img-lazy src="https://placekitten.com/480/210" alt="Image" height="420"></b-card-img-lazy>
+            <b-card-img-lazy src="https://image.shutterstock.com/image-photo/large-beautiful-drops-transparent-rain-600w-668593321.jpg" alt="Image" height="200"></b-card-img-lazy>
             <b-card-text>Header and footers variants.</b-card-text>
             <!-- <b-card-footer>This is a footer</b-card-footer> -->
         </b-card>
-    </div>
-    <div class="mt-3">
-        <b-card-group deck>
-            <b-card  >
-                <b-card-title>Card Title</b-card-title>
-                <b-card-img-lazy src="https://placekitten.com/480/210" alt="Image" height="200"></b-card-img-lazy>
-                <b-card-text>Header and footers variants.</b-card-text>
-                <!-- <b-card-footer>This is a footer</b-card-footer> -->
-            </b-card>
-
-           <b-card  >
-                <b-card-title>Card Title</b-card-title>
-                <b-card-img-lazy src="https://placekitten.com/480/210" alt="Image" height="200"></b-card-img-lazy>
-                <b-card-text>Header and footers variants.</b-card-text>
-                <!-- <b-card-footer>This is a footer</b-card-footer> -->
-            </b-card>
-        </b-card-group>
-    </div>
-
-    <div class="mt-3">
-        <b-card-group deck>
-            <b-card  >
-                <b-card-title>Card Title</b-card-title>
-                <b-card-img-lazy src="https://placekitten.com/480/210" alt="Image" height="200"></b-card-img-lazy>
-                <b-card-text>Header and footers variants.</b-card-text>
-                <!-- <b-card-footer>This is a footer</b-card-footer> -->
-            </b-card>
-
-           <b-card  >
-                <b-card-title>Card Title</b-card-title>
-                <b-card-img-lazy src="https://placekitten.com/480/210" alt="Image" height="200"></b-card-img-lazy>
-                <b-card-text>Header and footers variants.</b-card-text>
-                <!-- <b-card-footer>This is a footer</b-card-footer> -->
-            </b-card>
-        </b-card-group>
-    </div>
-
-    <div class="mt-3">
-        <b-card-group deck>
-            <b-card  >
-                <b-card-title>Card Title</b-card-title>
-                <b-card-img-lazy src="https://placekitten.com/480/210" alt="Image" height="200"></b-card-img-lazy>
-                <b-card-text>Header and footers variants.</b-card-text>
-                <!-- <b-card-footer>This is a footer</b-card-footer> -->
-            </b-card>
-
-           <b-card  >
-                <b-card-title>Card Title</b-card-title>
-                <b-card-img-lazy src="https://placekitten.com/480/210" alt="Image" height="200"></b-card-img-lazy>
-                <b-card-text>Header and footers variants.</b-card-text>
-                <!-- <b-card-footer>This is a footer</b-card-footer> -->
-            </b-card>
-        </b-card-group>
-    </div>
-
-    <div class="mt-3">
-        <b-card-group deck>
-            <b-card  >
-                <b-card-title>Card Title</b-card-title>
-                <b-card-img-lazy src="https://placekitten.com/480/210" alt="Image" height="200"></b-card-img-lazy>
-                <b-card-text>Header and footers variants.</b-card-text>
-                <!-- <b-card-footer>This is a footer</b-card-footer> -->
-            </b-card>
-
-           <b-card  >
-                <b-card-title>Card Title</b-card-title>
-                <b-card-img-lazy src="https://placekitten.com/480/210" alt="Image" height="200"></b-card-img-lazy>
-                <b-card-text>Header and footers variants.</b-card-text>
-                <!-- <b-card-footer>This is a footer</b-card-footer> -->
-            </b-card>
-        </b-card-group>
-    </div>
-
-    <div class="mt-3">
-        <b-card-group deck>
-            <b-card  >
-                <b-card-title>Card Title</b-card-title>
-                <b-card-img-lazy src="https://placekitten.com/480/210" alt="Image" height="200"></b-card-img-lazy>
-                <b-card-text>Header and footers variants.</b-card-text>
-                <!-- <b-card-footer>This is a footer</b-card-footer> -->
-            </b-card>
-
-           <b-card  >
-                <b-card-title>Card Title</b-card-title>
-                <b-card-img-lazy src="https://image.shutterstock.com/image-photo/large-beautiful-drops-transparent-rain-600w-668593321.jpg" alt="Image" height="200"></b-card-img-lazy>
-                <b-card-text>Header and footers variants.</b-card-text>
-                <!-- <b-card-footer>This is a footer</b-card-footer> -->
-            </b-card>
-        </b-card-group>
-    </div>
+    </b-card-group>
+  </div>
 </div>
 </template>
 
@@ -106,8 +106,6 @@ export default {
 }
 </script>
 
-<script>
-export default {
-    
-}
-</script>
+<style lang="stylus" scoped>
+
+</style>
--- resources/js/components/CNavLink.vue
+++ resources/js/components/CNavLink.vue
@@ -1,20 +1,20 @@
 <template>
 <div>
-    <!-- Left Side Of Navbar -->
-    <ul class="navbar-nav mr-auto">
-        <!-- Authentication Links -->
-        <li class="nav-item">
-            <a class="nav-link" href="login">Login</a>
-        </li>
-    </ul>
+  <!-- Left Side Of Navbar -->
+  <ul class="navbar-nav mr-auto">
+    <!-- Authentication Links -->
+    <li class="nav-item">
+        <a class="nav-link" href="login">Login</a>
+    </li>
+  </ul>
 
-    <!-- Right Side Of Navbar -->
-    <ul class="navbar-nav ml-auto">
-        <!-- Authentication Links -->
-        <li class="nav-item">
-            <a class="nav-link" href="login">Login</a>
-        </li>
-    </ul>
+  <!-- Right Side Of Navbar -->
+  <ul class="navbar-nav ml-auto">
+    <!-- Authentication Links -->
+    <li class="nav-item">
+        <a class="nav-link" href="login">Login</a>
+    </li>
+  </ul>
 </div>
 </template>
 
