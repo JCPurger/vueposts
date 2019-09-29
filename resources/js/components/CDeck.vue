f62ac92420d7131f3f542b8877b03db3cf43a0b1
JCPurger <jc.purger@gmail.com>
2019-09-29 19:09:41 -0300

    style components fix

--- resources/js/components/CDeck.vue
+++ resources/js/components/CDeck.vue
@@ -1,42 +1,36 @@
 <template>
-<div>
-  <div>
-    <div class="mt-3">
-      <b-pagination-nav 
+<div class="mt-3">
+    <b-pagination-nav 
         limit="6" 
         :link-gen="linkGen" 
         :number-of-pages="pages" 
         use-router 
         align="fill">
-      </b-pagination-nav>
-    </div>
-    
-    <div class="mt-3" v-for="(chunk,index) in postsChunked" :key="chunk.id">
-      <b-card v-on:click="redirect(chunk[0].slug)" v-if="index == 0">
-        <b-card-title>{{chunk[0].title}}</b-card-title>
-        <b-card-img-lazy v-bind="imgProps" :src="chunk[0].image" alt="Image" ></b-card-img-lazy>
-        <b-card-text>{{chunk[0].text}}</b-card-text>
-        <!-- <b-card-footer>This is a footer</b-card-footer> -->
-      </b-card>
-      <b-card-group deck v-else>
-          <b-card v-on:click="redirect(post.slug)" v-for="post in chunk" :key="post.id">
+    </b-pagination-nav>
+
+    <div class="my-3" v-for="(chunk,index) in postsChunked" :key="chunk.id">
+        <b-card v-on:click="redirect(chunk[0].slug)" v-if="index == 0">
+            <b-card-title>{{chunk[0].title}}</b-card-title>
+            <b-card-img-lazy v-bind="imgProps" :src="chunk[0].image" alt="Image" ></b-card-img-lazy>
+            <b-card-text>{{chunk[0].text}}</b-card-text>
+            <!-- <b-card-footer>This is a footer</b-card-footer> -->
+        </b-card>
+        <b-card-group deck v-else>
+            <b-card v-on:click="redirect(post.slug)" v-for="post in chunk" :key="post.id">
             <b-card-title>{{post.title}}</b-card-title>
             <b-card-img-lazy v-bind="imgProps" :src="post.image" alt="Image" ></b-card-img-lazy>
             <b-card-text>{{post.text}}</b-card-text>
-          </b-card>
-      </b-card-group>
+            </b-card>
+        </b-card-group>
     </div>
 
-    <div class="mt-3">
-      <b-pagination-nav 
+    <b-pagination-nav 
         limit="6" 
         :link-gen="linkGen" 
         :number-of-pages="pages" 
         use-router 
         align="fill">
-      </b-pagination-nav>
-    </div>
-  </div>
+    </b-pagination-nav>
 </div>
 </template>
 
@@ -64,12 +58,12 @@ export default {
     },
   },
   methods: {
-      redirect: function(slug) {
-          window.location = '/posts/' + slug;
-      },
-      linkGen(pageNum) {
-        return pageNum === 1 ? '?' : `?page=${pageNum}`
-      }
+    redirect: function(slug) {
+      window.location = '/posts/' + slug;
+    },
+    linkGen(pageNum) {
+      return pageNum === 1 ? '?' : `?page=${pageNum}`
+    }
   }
 }
 </script>
